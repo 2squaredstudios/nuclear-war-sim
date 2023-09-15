807c89d822afeb6c2e43dc92ee3ec08121306356
Lennon McLean <lmmclean08@gmail.com>
2023-09-15 15:02:50 -0400

    cursor should now work

--- game.js
+++ game.js
@@ -15,8 +15,8 @@ function loop() {
     // draw cursor
     ctx.fillStyle = 'green';
     ctx.strokeStyle = 'green';
-    ctx.fillRect(cursor.x - 10, cursor.y - 2, 20, 5);
-    ctx.fillRect(cursor.x - 2, cursor.y - 10, 5, 20);
+    ctx.fillRect(cursor.x - 20, cursor.y - 2, 40, 5);
+    ctx.fillRect(cursor.x - 2, cursor.y - 20, 5, 40);
     ctx.beginPath();
     ctx.arc(cursor.x, cursor.y, 10, 0, Math.PI * 2);
     ctx.stroke();
@@ -25,7 +25,7 @@ function loop() {
 loop();
 
 document.querySelector('canvas').onmousemove = function(event) {
-    var rect = canvas.getBoundingClientRect();
+    var rect = document.querySelector('canvas').getBoundingClientRect();
     cursor.x = event.clientX - rect.left;
     cursor.y = event.clientY - rect.top;
 }
