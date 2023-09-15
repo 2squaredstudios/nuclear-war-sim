3f4ca8fb9b6cafd1368081129652cea470105fdc
Lennon McLean <lmmclean08@gmail.com>
2023-09-15 14:50:41 -0400

    fix bug

--- game.js
+++ game.js
@@ -2,6 +2,8 @@ var ctx = document.querySelector('canvas').getContext('2d');
 var bombs = [{x: 500, y: 500}];
 var cursor = {x: 0, y:0}
 function loop() {
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, 1920, 720)
     // draw bombs
     for (var i = 0; i < bombs.length; i++) {
         ctx.fillStyle = 'red';
@@ -10,12 +12,13 @@ function loop() {
         ctx.fill();
     }
     // draw cursor
+    ctx.fillStyle = 'black';
     ctx.fillRect(cursor.x, cursor.y, 10, 10);
     requestAnimationFrame(loop);
 }
 loop();
 
-document.querySelector('canvas').onmouseover = function(event) {
-    cursor.x = event.eventX;
-    cursor.y = event.eventY;
+document.querySelector('canvas').onmousemove = function(event) {
+    cursor.x = event.clientX;
+    cursor.y = event.clientY;
 }
