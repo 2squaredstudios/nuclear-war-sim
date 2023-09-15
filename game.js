4194282651b60f086b0e6ec4f3a1c45a419de235
Lennon McLean <lmmclean08@gmail.com>
2023-09-15 15:00:37 -0400

    cursor should work

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 var ctx = document.querySelector('canvas').getContext('2d');
 var bombs = [{x: 500, y: 500}];
-var cursor = {x: 0, y:0}
+var cursor = {x: 0, y:0};
+
 function loop() {
     ctx.fillStyle = 'white';
     ctx.fillRect(0, 0, 1920, 720)
@@ -12,13 +13,19 @@ function loop() {
         ctx.fill();
     }
     // draw cursor
-    ctx.fillStyle = 'black';
-    ctx.fillRect(cursor.x, cursor.y, 10, 10);
+    ctx.fillStyle = 'green';
+    ctx.strokeStyle = 'green';
+    ctx.fillRect(cursor.x - 10, cursor.y - 2, 20, 5);
+    ctx.fillRect(cursor.x - 2, cursor.y - 10, 5, 20);
+    ctx.beginPath();
+    ctx.arc(cursor.x, cursor.y, 10, 0, Math.PI * 2);
+    ctx.stroke();
     requestAnimationFrame(loop);
 }
 loop();
 
 document.querySelector('canvas').onmousemove = function(event) {
-    cursor.x = event.clientX;
-    cursor.y = event.clientY;
+    var rect = canvas.getBoundingClientRect();
+    cursor.x = event.clientX - rect.left;
+    cursor.y = event.clientY - rect.top;
 }
